e4bb27bed5a9f36db401e0ad7d03a7d98ae51903
Cata-8 <catalinaalmuna01@gmail.com>
2026-09-07 21:36:22 -0300

    agregar css

--- login.js
+++ login.js
@@ -3,6 +3,22 @@ function ingresar(){
     let correo = document.getElementById("correo").value;
     let clave = document.getElementById("clave").value;
 
+    if (correo === "" || clave === "") {
+        alert("Debe completar todos los campos");
+        return;
+    }
+
+    let formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!formatoCorreo.test(correo)) {
+        alert("Ingrese un correo válido");
+        return;
+    }
+
+    if (clave.length !== 4) {
+        alert("La clave debe tener 4 dígitos");
+        return;
+    }
 
     if (correo === "admin@demo.cl" && clave === "1234") {
         window.location.href = "admin.html";
